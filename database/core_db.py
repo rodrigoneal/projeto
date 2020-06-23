a7df654df42fbb8311297261bb2f279bf70cc998
rodrigho2006@gmail.com <f47fc32FTMzUvFD>
2020-06-22 21:39:45 -0300

    Organização

--- database/core_db.py
+++ database/core_db.py
@@ -1,14 +1,13 @@
 import os
 
 
-
-def apagar(path):
+@property
+def _apagar(path):
     dir = '/Users/rodrgo/PycharmProjects/projeto/sql'
     os.remove(f'{dir}/{path}.sql')
 
 
-def criar_sql(sql, tabela_nome: str, opcao: str ='inserir'.lower()) ->str:
-    if opcao == 'criar':
+def criar_sql(sql, tabela_nome: str) ->str:
         with open(f'sql/{tabela_nome}.sql', 'w') as arquivo:
             virgula = len(sql)
             cont = 1
@@ -20,32 +19,34 @@ def criar_sql(sql, tabela_nome: str, opcao: str ='inserir'.lower()) ->str:
                     arquivo.write(f'{i},\n')
                 cont += 1
             arquivo.write('\n);')
-        with open(f'sql/{tabela_nome}.sql', 'rt') as sql:
-            schema = sql.read()
-        apagar(tabela_nome)
-        return schema
-    elif opcao == 'inserir':
-        with open(f'sql/{tabela_nome}.sql', 'w') as arquivo:
-            arquivo.write(f'INSERT INTO {tabela_nome} (')
-            virgula = len(sql)
-            cont = 1
-            for i in sql:
-                if cont == virgula:
-                    arquivo.write(f'{i}) ')
-                else:
-                    arquivo.write(f'{i}, ')
-                cont += 1
+            with open(f'sql/{tabela_nome}.sql', 'rt') as sql:
+                schema = sql.read()
+            _apagar(tabela_nome)
+            return schema
 
-            arquivo.write('Values (')
-            cont = 1
-            for i in sql:
-                if cont == virgula:
 
-                    arquivo.write(f'"{sql[i]}"); ')
-                else:
-                    arquivo.write(f'"{sql[i]}", ')
-                cont += 1
-        with open(f'sql/{tabela_nome}.sql', 'rt') as sql:
-            schema = sql.read()
-        apagar(tabela_nome)
-        return schema
+def inserir_sql(sql: str, tabela_nome: str):
+    with open(f'sql/{tabela_nome}.sql', 'w') as arquivo:
+        arquivo.write(f'INSERT INTO {tabela_nome} (')
+        virgula = len(sql)
+        cont = 1
+        for i in sql:
+            if cont == virgula:
+                arquivo.write(f'{i}) ')
+            else:
+                arquivo.write(f'{i}, ')
+            cont += 1
+
+        arquivo.write('Values (')
+        cont = 1
+        for i in sql:
+            if cont == virgula:
+
+                arquivo.write(f'"{sql[i]}"); ')
+            else:
+                arquivo.write(f'"{sql[i]}", ')
+            cont += 1
+    with open(f'sql/{tabela_nome}.sql', 'rt') as sql:
+        schema = sql.read()
+    _apagar(tabela_nome)
+    return schema
