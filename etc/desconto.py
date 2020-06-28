d3c5c411d82ec71e9b7cdf106b5f931ef0b5f502
rodrigho2006@gmail.com <f47fc32FTMzUvFD>
2020-06-28 07:14:42 -0300

    corrigido o problema que a data era calculada errada e inserido a função de inserir logo no banco de dados

--- etc/desconto.py
+++ etc/desconto.py
@@ -1,16 +1,24 @@
 import inicializar_tabelas
 from database.core_db import Manipular_sql
+from setup import setup
+from .calendario import meses
 
-datas = inicializar_tabelas.__datas__
 
-def desc(valor, tempo_sem):
-    preco = valor
+def desc(tempo_sem):
+    preco = setup.ler_setup()['internet']
     dia = preco / 30
     hora = dia / 24
-    minutos = round(hora / 60, 3)
-    print(minutos)
+    minutos = (hora / 60)
     segundos = minutos / 60
     desc = tempo_sem * segundos
     return desc
 
-def inserir():
+
+def inserir_desconto(data: str, tempo_sem):
+    slice = data[0:2]
+    data_inserir = inicializar_tabelas.__datas__
+    mes = meses()[slice]
+    inserir = Manipular_sql(data_inserir)
+    desconto = desc(tempo_sem)
+    inserir.criar_inserir_sql({mes: desconto})
+    inserir.executar_sql()
